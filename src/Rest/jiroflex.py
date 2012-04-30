43a6c9a4a34d145bd4b454ae0ffb3d4fb6e6803a
Vinícius Krolow <vkrolow@conrad-caine.com>
2012-04-30 11:20:29 -0300

    listen all ips

--- src/Rest/jiroflex.py
+++ src/Rest/jiroflex.py
@@ -5,11 +5,9 @@ from jira import jira
 app = Flask(__name__)
 client = jira.Jira(CONFIG['url'], CONFIG['username'], CONFIG['password'])
 
-app.debug = True
-
 @app.route("/progress/<username>")
 def progress(username):
     return jsonify(progress=client.has_task_in_progress(username))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
